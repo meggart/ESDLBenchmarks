7388cbce7cc1a61ba9d2269bb06c699add512f9a
meggart <fabiangans@gmx.de>
2018-08-10 10:17:48 +0200

    added plotting notebook

--- julia.jl
+++ julia.jl
@@ -1,11 +1,15 @@
 using OnlineStats
 using ESDL
+using Zarr
+using ESDLZarr
 
 function runbenchmark(cubedir;max_cache=1e7)
   
-  tim = Dict{String,Float64}()
+  tim = Dict{String,Any}()
+  tim["cube"]=cubedir
+  tim["cache_size"]=max_cache
   tim["open"] = @elapsed begin
-    c = Cube(cubedir)
+    c = endswith(cubedir,"zarr") ? ESDLZarr.ZarrCube(cubedir) : Cube(cubedir)
     d=getCubeData(c,variable = ["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"],longitude=(-10,50),latitude=(35,65))
   end
   tim["map_access"] = @elapsed a = d[:,:,100,:]
@@ -23,11 +27,24 @@ end
 tim_precompile = runbenchmark("/scratch/DataCube/v1.0.0/low-res")
 tim_old = runbenchmark("/scratch/DataCube/v1.0.0/low-res")
 tim_compressed_map = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1")
+tim_zarr_map = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1_zarr")
 tim_compressed_ts = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-46x90x90-1.0.1_2")
 
 #Now with more memory
 tim_old_c = runbenchmark("/scratch/DataCube/v1.0.0/low-res",max_cache=1e8)
 tim_compressed_map_c = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1",max_cache=1e8)
+tim_zarr_map_c = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1_zarr",max_cache=1e8)
 tim_compressed_ts_c = runbenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-46x90x90-1.0.1_2",max_cache=1e8)
+
+all_tims = [tim_old, tim_compressed_map,tim_compressed_ts,tim_old_c,tim_compressed_map_c,tim_compressed_ts_c,tim_zarr_map, tim_zarr_map_c]
+colnames = collect(keys(all_tims[1]))
+v = map(colnames) do k
+  [d[k] for d in all_tims]
+end
+using DataFrames
+using CSV
+df = DataFrame(Dict(zip(colnames,v)))
+CSV.write("julia_benchmarks.csv",df,header=true)
+  
 
 
--- python.py
+++ python.py
@@ -7,9 +7,14 @@ from timeit import timeit
 def ESDLBenchmark(cubepath): 
   
   def openfun():
-    cube = Cube.open(cubepath)
-    ds =   cube.data.dataset(key=["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"])
-    ds = ds[["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"]].sel(lon=slice(-10,50),lat=slice(65,35))
+    if cubepath.endswith("zarr"):
+      cube=xr.open_zarr(cubepath)
+      ds = cube[["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"]].sel(lon=slice(-10,50),lat=slice(65,35))
+    else:
+      cube = Cube.open(cubepath)
+      ds =   cube.data.dataset(key=["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"])
+      ds = ds[["gross_primary_productivity","net_ecosystem_exchange","terrestrial_ecosystem_respiration"]].sel(lon=slice(-10,50),lat=slice(65,35))
+    
     return(ds)
 
   ds = openfun()
@@ -41,13 +46,13 @@ def ESDLBenchmark(cubepath):
   tim["ts_access"]  = timeit(tsaccessfun,number=1)
   tim["msc"]        = timeit(mscfun,number=1)
   tim["removeMSC"]  = timeit(removemscfun, number=1)
-  tim["spatial_mean"] = timeit()
+  tim["spatial_mean"] = timeit(spatialmeanfun,number=1)
   return tim
 
-tim_old = ESDLBenchmark("/scratch/DataCube/v1.0.0/low-res/")
-tim_compressed_map = ESDLBenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1/")
-tim_compressed_ts  = ESDLBenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-46x90x90-1.0.1_2/")
-
+tim_old = ESDLBenchmark("/scratch/DataCube/v1.0.0/low-res")
+tim_compressed_map = ESDLBenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1")
+tim_compressed_ts  = ESDLBenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-46x90x90-1.0.1_2")
+#tim_zarr           = ESDLBenchmark("/scratch/DataCube/v1.0.1/esdc-8d-0.25deg-1x720x1440-1.0.1_1_zarr")
 
 all_timings = [tim_old,tim_compressed_map,tim_compressed_ts]
 import csv
